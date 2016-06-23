465559727c66ae4d40fe5195ebdb30c426d6a379
Jujunol <jujunol@outlook.com>
2016-06-22 21:12:27 -0400

    Added Delete functionality to Games

---
+++ comp2007-s2016-team-proj/UserMenu/GameDelete.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="GameDelete.aspx.cs" Inherits="comp2007_s2016_team_proj.UserMenu.GameDelete" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ comp2007-s2016-team-proj/UserMenu/GameDelete.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using comp2007_s2016_team_proj.Models;
+
+namespace comp2007_s2016_team_proj.UserMenu
+{
+    public partial class GameDelete : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Request.QueryString.Count > 0)
+            {
+                int gameID = Convert.ToInt32(Request.QueryString["GameID"]);
+                using (BaseTrackerConnection db = new BaseTrackerConnection())
+                {
+                    Game game = (from gameList in db.Games
+                                 where gameList.GameID == gameID
+                                 select gameList).FirstOrDefault();
+
+                    if(game != null)
+                    {
+                        db.Games.Remove(game);
+                        db.SaveChanges();
+                    }
+                }
+            }
+            Response.Redirect("/MainGamePage.aspx");
+        }
+    }
+}
