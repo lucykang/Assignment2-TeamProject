54df937b1720bd999cfb8ea5f72d3d8679adcd76
Lucy Kang <HaeYeon.Kang@MyGeorgian.ca>
2016-08-09 17:26:46 -0400

    Restored db connections.

--- comp2007-s2016-team-proj/Admin/Users.aspx.cs
+++ comp2007-s2016-team-proj/Admin/Users.aspx.cs
@@ -12,6 +12,7 @@ using System.Web.ModelBinding;
 
 namespace comp2007_s2016_team_proj
 {
+    
     public partial class Users : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
--- comp2007-s2016-team-proj/Register.aspx.cs
+++ comp2007-s2016-team-proj/Register.aspx.cs
@@ -15,6 +15,7 @@ namespace comp2007_s2016_team_proj
 {
     public partial class Register : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
